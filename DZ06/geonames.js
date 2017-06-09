2b82bd18273556ffbda5608011e70cc80ae383f5
petarpetrovic2913 <petar.petrovic.2913@metropolitan.ac.rs>
2017-06-09 10:11:06 -0700

    Delete geonames.js

--- DZ06/geonames.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "http://api.geonames.org/citiesJSON"
-    }).then(function(data) {
-       $('.citiesJSON-message').append(data.status.message);
-       $('.citiesJSON-value').append(data.status.value);
-    });
-});
